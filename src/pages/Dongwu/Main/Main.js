c9e90896f3da05f96464853d42ef0a5870194cd9
mateo <materokatti@gmail.com>
2021-07-21 20:46:23 +0900

    Add: add nav in Component folder

--- src/pages/Dongwu/Main/Main.js
+++ src/pages/Dongwu/Main/Main.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 class MainDongwu extends React.Component {
   render() {
--- src/pages/Jungwoo/Login/Login.js
+++ src/pages/Jungwoo/Login/Login.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 class LoginJungwoo extends React.Component {
   render() {
