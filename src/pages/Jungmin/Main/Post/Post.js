de21402ac6167ea85efba1e17d6242f18115d147
jungmin <minmin93240@google.com>
2021-07-26 20:53:19 +0900

    Modify: 댓글기능 - 이벤트 수정, -엔터, 클릭 후 input창 초기화 기능 추가 -NewComment key값 추가

--- src/pages/Jungmin/Main/Post/Post.js
+++ src/pages/Jungmin/Main/Post/Post.js
@@ -18,108 +18,32 @@ class Comment extends React.Component {
   }
 }
 
-// class Message extends React.Component {
-//   render() {
-//     return (
-//       <div className="message">
-//         <input
-//           name="name"
-//           type="text"
-//           className="textMessage username"
-//           placeholder="댓글 달기..."
-//           onKeyUp={this.props.change}
-//         />
-//         <button className="postButton" onClick={this.props.click} value="click">
-//           게시
-//         </button>
-//       </div>
-//     );
-//   }
-// }
+class NewComment extends React.Component {
+  render() {
+    const save = this.props.save;
+    const post = save.map((value, index) => (
+      <Comment nickname={index} comment={value} key={index + value} />
+    ));
+    return <>{post}</>;
+  }
+}
 
-// class NewComment extends React.Component {
-//   render() {
-//     const saveq = this.props.save;
-//     const post = saveq.map((value, index) => (
-//       <Comment nickname={index} comment={value} />
-//     ));
-//     return <>{post}</>;
-//   }
-// }
-
-// class Post extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       nickname: '',
-//       content: '',
-//       save: [],
-//     };
-//   }
-
-//   // sumitCommend = e => {
-//   //   if (e.target.value === 'click' || e.key === 'Enter') {
-//   //     this.setState({
-//   //       save: this.state.save.concat(this.state.content),
-//   //       content: '',
-//   //     });
-//   //     e.target.value = '';
-//   //   } else {
-//   //     this.setState({
-//   //       content: e.target.value,
-//   //     });
-//   //   }
-//   // };
-//   changeCommend = e => {
-//     console.log(e.target);
-//     this.setState({
-//       content: e.target.value,
-//     });
-//   };
-
-//   sumitCommend = e => {
-//     this.setState({
-//       save: this.state.save.concat(this.state.content),
-//     });
-//   };
-
-//   render() {
-//     console.log(this.state);
-//     return (
-//       <div className="post">
-//         <Comment nickname={'hh.vv'} comment={'오 멋진 걸'} />
-//         <Comment nickname={'seoyun1203'} comment={'하이 에이치 아이 !!'} />
-//         <NewComment save={this.state.save} />
-//         <div className="commendTime  username ">
-//           <div>42분 전</div>
-//         </div>
-//         <Message
-//           messageChangeState={this.changeState}
-//           click={this.sumitCommend}
-//           change={this.changeCommend}
-//         />
-//       </div>
-//     );
-//   }
-// }
-
-class Message extends React.Component {
+class Sumit extends React.Component {
   render() {
     return (
-      <form
-        className="message"
-        // onSubmit={this.props.click}
-        onSubmit={this.props.messageChangeState}
-      >
+      <form className="sumit" onSubmit={this.props.messageChangeState}>
         <input
-          name="name"
           type="text"
           className="textMessage username"
           placeholder="댓글 달기..."
-          // onKeyUp={this.props.change}
-          onKeyUp={this.props.changeCommend}
+          value={this.props.value}
+          onChange={this.props.changeCommend}
         />
-        <button className="postButton" type="submit">
+        <button
+          className="postButton"
+          type="submit"
+          style={this.props.value ? { opacity: 1 } : { opacity: 0.5 }}
+        >
           게시
         </button>
       </form>
@@ -127,83 +51,33 @@ class Message extends React.Component {
   }
 }
 
-class NewComment extends React.Component {
-  render() {
-    const saveq = this.props.save;
-    const post = saveq.map((value, index) => (
-      <Comment nickname={index} comment={value} />
-    ));
-    return <>{post}</>;
-  }
-}
-
 class Post extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      nickname: 0,
       content: '',
       save: [],
     };
   }
 
   changeCommend = e => {
-    console.log(e.target.value);
-    // this.state.content = '';
     this.setState({
       content: e.target.value,
     });
-    console.log(this.state.nickname, this.state.save.length);
-    console.log('렌더 후 ', this.state);
-    this.sumitCommend(e);
   };
 
   sumitCommend = e => {
     e.preventDefault();
+    console.log(e.target.value);
     if (e.target.value === undefined && this.state.content !== '') {
       this.setState({
         save: this.state.save.concat(this.state.content),
+        content: '',
       });
     }
-    if (this.state.nickname !== this.state.save.length) {
-      this.setState({
-        nickname: (this.state.nickname = this.state.nickname + 1),
-      });
-      e.target.value = '';
-    }
-    console.log('누구냐', e.target);
   };
 
-  // changeCommend = e => {
-  //   console.log(e.target.value);
-  //   // this.state.content = '';
-  //   this.setState({
-  //     content: e.target.value,
-  //   });
-
-  //   if (this.state.nickname === false) {
-  //     e.target.value = '';
-  //     this.setState({
-  //       nickname: !this.state.nickname,
-  //     });
-  //   }
-  // };
-
-  // sumitCommend = (e) => {
-  //   e.preventDefault();
-  //   this.setState({
-  //     save: this.state.save.concat(this.state.content),
-  //     nickname: !this.state.nickname,
-  //   });
-  // };
-
   render() {
-    // if (this.state.nickname !== this.state.save.length) {
-    //   this.setState({
-    //     nickname: (this.state.nickname = this.state.nickname + 1),
-    //   });
-    //   console.log('렌더 후 ', this.state);
-    // }
     return (
       <div className="post">
         <Comment nickname={'hh.vv'} comment={'오 멋진 걸'} />
@@ -212,10 +86,10 @@ class Post extends React.Component {
         <div className="commendTime  username ">
           <div>42분 전</div>
         </div>
-        <Message
+        <Sumit
           messageChangeState={this.sumitCommend}
+          value={this.state.content}
           changeCommend={this.changeCommend}
-          // change={this.changeCommend}
         />
       </div>
     );
